14f04ce32b84ceacd0daff2e4398825ab8390823
MohammedAlnajrani <m.alnajrani1999@gmail.com>
2023-06-19 12:41:58 +0300

    coderhup code and pythone101

---
+++ coderhup_string_builder.py
@@ -0,0 +1,9 @@
+def fun(letter,number):
+    i=0
+    while i < int(number):
+        print(letter)
+        i+=1    
+
+let=input("Enter letter: ")
+num=input("Enter number: ")
+fun(let,num)
---
+++ python101.py
@@ -0,0 +1,56 @@
+# input_name=input("Enter you name: ")
+
+# print("Hi, ",input_name)
+
+#////////////list////////////////////////
+
+# list_name=["ahmed","mohammed","khlied","saud",1999]
+# print(list_name)
+# print(len(list_name))
+
+# list_name.append('muslih')
+# print(list_name)
+# print(len(list_name)," after add append")
+
+# list_name.insert(8,1118)
+# print(list_name)
+# print(len(list_name)," after insert")
+
+#/////////////dictionary/////////////////////
+# child_info={'Name': 'mohammed', 'DOB':'1999','Nationality': 'saudi'}
+# print(child_info)
+# print(child_info.keys())
+# print(child_info.values())
+
+#////////////for loo[///////////////
+# for l in "Welcome to Python":
+#     print(l)
+
+#//////////////return/////////////////
+# A Python program to return multiple
+# values from a method using tuple
+
+# This function returns a tuple
+# def fun():
+# 	str = "geeksforgeeks"
+# 	x = 20
+# 	return str, x; # Return tuple, we could also
+# 					# write (str, x)
+
+# # Driver code to test above method
+# str, x = fun() # Assign returned tuple
+# print(str)
+# print(x)
+# print(fun())
+
+
+#//////////////////
+# def my_func(number):
+#     print(number * number)
+#     return number
+
+
+# my_func(2)#=4
+# print(my_func(2))#=4 becuse function print and it will also print 2 becuse return = 2
+
+#//////////////////////////////////
